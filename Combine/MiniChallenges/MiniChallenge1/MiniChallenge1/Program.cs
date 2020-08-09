67b2171a8871d6de1e29696e3960b3e14d6a3904
pagtamajr@gmail.com <pagtamajr@gmail.com>
2020-08-09 11:08:36 -0700

    Combine MiniChallenge1

---
+++ Combine/MiniChallenges/MiniChallenge1/MiniChallenge1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MiniChallenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            Console.WriteLine($"Hello, {Console.ReadLine()}");
+        }
+    }
+}
