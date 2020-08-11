a33856413ec2d61307b3eb639e4ec4621716abdb
pagtamajr@gmail.com <pagtamajr@gmail.com>
2020-08-10 20:29:46 -0700

    Combine MiniChallenge4

---
+++ Combine/MiniChallenges/MiniChallenge4/MiniChallenge4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MiniChallenge4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            string isGreaterOrLess;
+
+            Console.WriteLine("Enter 2 numbers: ");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            isGreaterOrLess = num1 > num2 ? $"{num1} > {num2}" : $"{num1} < {num2}";
+
+            Console.WriteLine($"{isGreaterOrLess}");
+        }
+    }
+}
