25d4b1c0677f3f9a3121d3028009a55af9e6a265
pagtamajr@gmail.com <pagtamajr@gmail.com>
2020-08-10 20:01:11 -0700

    Combine MiniChallenge3

---
+++ Combine/MiniChallenges/MiniChallenge3/MiniChallenge3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MiniChallenge3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, time;
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("What time did you wake up?");
+            time = Console.ReadLine();
+
+            Console.WriteLine($"{name} woke up at {time}");
+        }
+    }
+}
