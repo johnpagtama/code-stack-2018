cd0a398f2062d2f0de1ec0faf63f9aef71b902ee
pagtamajr@gmail.com <pagtamajr@gmail.com>
2020-08-11 14:17:05 -0700

    Combine MiniChallenge6

---
+++ Combine/MiniChallenges/MiniChallenge6/MiniChallenge6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MiniChallenge6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number: ");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            string isOdd = (num % 2 == 0 ? "is Even" : "is Odd");
+
+            Console.WriteLine($"{num} {isOdd}");
+        }
+    }
+}
