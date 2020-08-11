da226a274b364671d7af0bcdf9eb3cb26f49a218
pagtamajr@gmail.com <pagtamajr@gmail.com>
2020-08-10 19:49:36 -0700

    Combine MiniChallenge2

---
+++ Combine/MiniChallenges/MiniChallenge2/MiniChallenge2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MiniChallenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+
+            Console.WriteLine("Enter 2 numbers: ");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = num1 + num2;
+
+            Console.WriteLine($"{num1} plus {num2} equals {num3}");
+        }
+    }
+}
